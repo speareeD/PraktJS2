770fc8b435aadda708eaa91e4a1626d9be20af1b
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-04-10 14:14:51 +0300

    Added new contoller with index action

---
+++ UIRender/Controllers/UIDemoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UIRender.Controllers;
+
+public class UIDemoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
