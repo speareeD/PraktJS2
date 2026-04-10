9b70f2ae62d8e4fa29e584a424398479c07f57f0
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-04-10 15:08:10 +0300

    Added UIButton class with UIComponent base class to create a button on the page

--- UIRender/wwwroot/Scripts/ui-components.js
+++ UIRender/wwwroot/Scripts/ui-components.js
@@ -1,6 +1,8 @@
 ﻿class UIComponent {
     id;
     cssClass;
+    element = document.createElement(this.render());
+    
     constructor(id, cssClass) {
         this.id = id;
         this.cssClass = cssClass;
@@ -11,12 +13,32 @@
     
     mount(containerId) {
         const container = document.getElementById(containerId);
-        const element = document.createElement(this.render());
-        element.id = this.id;
-        container.appendChild(element);
+        this.element.id = this.id;
+        this.element.classList.add(this.cssClass);
+        container.appendChild(this.element);
     }
 }
 
+class UIButton extends UIComponent {
+    text;
+    color;
+    constructor(id, cssClass, text, color) {
+        super(id, cssClass);
+        this.text = text;
+        this.color = color;
+    }
+    render() {
+        return 'button';
+    }
+    mount(containerId) {
+        super.mount(containerId);
+        this.element.innerHTML = this.text;
+        this.element.style.backgroundColor = this.color;
+    }
+}
 
-let component = new UIComponent('newComp', 'comp');
+let component = new UIComponent('container', 'container');
+let component1 = new UIButton('js-button', 'btn', 'Click me!', 'green');
+
 component.mount('app');
+component1.mount('container');
