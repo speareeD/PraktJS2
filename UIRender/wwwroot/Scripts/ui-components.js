da26cf70a537643324a2dc86d6582ca9c98193b0
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-04-10 14:53:35 +0300

    Added base class UIComponent

---
+++ UIRender/wwwroot/Scripts/ui-components.js
@@ -0,0 +1,22 @@
+﻿class UIComponent {
+    id;
+    cssClass;
+    constructor(id, cssClass) {
+        this.id = id;
+        this.cssClass = cssClass;
+    }
+    render() {
+        return 'div';
+    }
+    
+    mount(containerId) {
+        const container = document.getElementById(containerId);
+        const element = document.createElement(this.render());
+        element.id = this.id;
+        container.appendChild(element);
+    }
+}
+
+
+let component = new UIComponent('newComp', 'comp');
+component.mount('app');
